136b7679ea26d74c2b3447b4d4f389e4dfa64aab
Togrul Mageramov <cmeisters@gmail.com>
2015-07-14 18:03:27 +0300

    * Added Ember DashboardController

--- grails-app/assets/javascripts/router.js
+++ grails-app/assets/javascripts/router.js
@@ -27,6 +27,24 @@ OlaMagic.Router.reopen({
     location: 'auto'
 });
 
+OlaMagic.DashboardController = Ember.ObjectController.extend({
+    isShowMissionControl: false,
+    isShowSiteControl: false,
+    currentSite: "Site...",
+    actions: {
+        showMissionControl: function () {
+            this.set('isShowMissionControl', !this.isShowMissionControl);
+        },
+        showSiteControl: function () {
+            this.set('isShowSiteControl', !this.isShowSiteControl);
+        },
+        selectSite: function (value) {
+            this.set('currentSite', value);
+            this.set('isShowSiteControl', false);
+        }
+    }
+});
+
 
 OlaMagic.DashboardPbxNumbers1Route = Ember.Route.extend({
 
