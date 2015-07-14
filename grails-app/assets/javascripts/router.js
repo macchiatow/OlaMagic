abc2bfc96c38960f5174d214bcf161f299dca339
Togrul Mageramov <cmeisters@gmail.com>
2015-07-14 09:37:11 +0300

    * Added more Ember.js ready pages

--- grails-app/assets/javascripts/router.js
+++ grails-app/assets/javascripts/router.js
@@ -1,11 +1,33 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 OlaMagic.Router.map(function() {
-    this.resource('dashboard', { path: '/dashboard/:uid' }, function() {
-        this.route('pbx.numbers' , {path: '/numbers'});
+    this.route('dashboard', { path: '/dashboard/:uid' }, function() {
+        this.route('pbx', function() {
+            this.route('numbers', {path : '/numbers'});
+        });
+        this.route('reports', function() {
+            this.route('report-one');
+            this.route('report-two');
+            this.route('report-three');
+        });
+        this.route('reports', function() {
+            this.route('report-one');
+            this.route('report-two');
+            this.route('report-three');
+        });
+        this.route('analytics', function() {
+            this.route('campaigns');
+            this.route('adsources');
+        });
+        this.route('sites', {path : '/sitez'});
     });
 });
 
 OlaMagic.Router.reopen({
-    location: 'history'
+    location: 'auto'
+});
+
+
+OlaMagic.DashboardPbxNumbers1Route = Ember.Route.extend({
+
 });
