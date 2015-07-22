b192d194713b5e4d0da75680b52b31748a537fab
Togrul Mageramov <cmeisters@gmail.com>
2015-07-22 09:45:15 +0300

    * Re-added adapters

---
+++ ember-app/app/adapters/workspace_adapter.js
@@ -0,0 +1,23 @@
+/*
+import DS from 'ember-data';
+
+export default DS.RESTAdapter.extend({
+    namespace : 'api',
+    host : 'http://localhost:8080/',
+    urlForQuery: function(query, modelName){
+        console.log('hihi');
+
+        var url = ['api','users', query.user, modelName+'s'];
+        delete query.user;
+        var host = 'http://localhost:8080/';
+        var prefix = this.urlPrefix();
+
+        url = url.join('/');
+        if (!host && url && url.charAt(0) !== '/') {
+            url = '/' + url;
+        }
+        console.log(host+url);
+
+        return host+url;
+    }
+});*/
