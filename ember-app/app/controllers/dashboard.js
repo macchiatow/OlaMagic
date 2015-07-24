baa6dd3c7e2c8425ebbf0618dc6dc25f9acff676
Togrul Mageramov <cmeisters@gmail.com>
2015-07-24 09:48:02 +0300

    * Fixed site-control

--- ember-app/app/controllers/dashboard.js
+++ ember-app/app/controllers/dashboard.js
@@ -1,19 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  navigatorVisible: "",
-  isShowSiteControl: false,
-  currentSite: "Site...",
-  actions: {
-      showMissionControl: function () {
-          this.set('navigatorVisible', (this.navigatorVisible == "") ? "navigator-visible" : "");
-      },
-      showSiteControl: function () {
-          this.set('isShowSiteControl', !this.isShowSiteControl);
-      },
-      selectSite: function (value) {
+    navigatorVisible: "",
+    siteSectionVisible: "",
+    currentSite: "Site...",
+    
+    actions: {
+        showMissionControl: function () {
+            this.set('navigatorVisible', (this.navigatorVisible == "") ? "navigator-visible" : "");
+        },
+        
+        showSiteControl: function () {
+            this.set('siteSectionVisible', (this.siteSectionVisible == "") ? "open" : "");
+        },
+        
+        selectSite: function (value) {
           this.set('currentSite', value);
           this.set('isShowSiteControl', false);
-      }
-  }
+        }
+    }
 });
