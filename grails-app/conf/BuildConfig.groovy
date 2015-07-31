e3a609cc851e3d0c9678bfd6de069df720fe55c1
Togrul Mageramov <cmeisters@gmail.com>
2015-07-31 09:21:49 +0300

    * Added test-coverage plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -60,6 +60,7 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.55.2" // or ":tomcat:8.0.20"
 
         // plugins for the compile step
+        compile ':spring-security-core:2.0-RC5'
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":ember-asset-pipeline:1.7.0"
@@ -69,11 +70,7 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
-        // Uncomment these to enable additional asset-pipeline capabilities
-        //compile ":sass-asset-pipeline:1.9.0"
-        //compile ":less-asset-pipeline:1.10.0"
-        //compile ":coffee-asset-pipeline:1.8.0"
-        //compile ":handlebars-asset-pipeline:1.3.0.3"
-        compile ':spring-security-core:2.0-RC5'
+        test ":code-coverage:2.0.3-3"
+
     }
 }
