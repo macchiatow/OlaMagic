a3640b6397caf745491a6bd1a9d3c6d88d780a07
Togrul Mageramov <cmeisters@gmail.com>
2015-08-09 15:54:15 +0300

    * Experimenting with createRecord

--- ember-app/app/controllers/dashboard/workspaces.js
+++ ember-app/app/controllers/dashboard/workspaces.js
@@ -4,14 +4,18 @@ export default Ember.Controller.extend({
 
     actions: {
         newWorkspace: function () {
-            var mod = this.get('model');
+            var model = this.get('model')
+            var self = this;
+
+            var onSuccess = function(workspace) {
+                model.addObject(workspace);
+                self.transitionToRoute('dashboard.workspaces.workspace', workspace.id);
+            };
 
             this.store.createRecord('workspace', {
                 title: 'Rails is Omakase'
-            }).save().then(function(m) {
-                mod.addObject(m);
-                console.log("Author name after: " + m.get("title"));
-            });
+            }).save().then(onSuccess);
+
         }
     }
 });
