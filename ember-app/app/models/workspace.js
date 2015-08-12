f5274aa3a2775283eb8479e59bfa7fb3d7695e3e
Togrul Mageramov <cmeisters@gmail.com>
2015-08-12 21:13:29 +0300

    * Added collaborators iterator to workspace.hbs

--- ember-app/app/models/workspace.js
+++ ember-app/app/models/workspace.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   owner: DS.attr('string'),
-  contributors: DS.attr('string'),
+  contributors: DS.attr(),
   numbersCount: DS.attr('number'),
   sitesCount: DS.attr('number'),
   adSourcesCount: DS.attr('number'),
