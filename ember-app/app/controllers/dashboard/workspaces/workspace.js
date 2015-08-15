f2e6c43ee3c88eb84b0bb4d843dbc67e00bf8e80
Togrul Mageramov <cmeisters@gmail.com>
2015-08-15 23:14:42 +0300

    * Handled 'Access' newContributor action

--- ember-app/app/controllers/dashboard/workspaces/workspace.js
+++ ember-app/app/controllers/dashboard/workspaces/workspace.js
@@ -16,8 +16,27 @@ export default Ember.Controller.extend({
 
         },
 
-        addContributor: function (email) {
-            console.log(email);
+        addContributor: function (email, wid) {
+            var self = this;
+
+            var refreshModel = function(newModel) {
+                self.set('model', newModel);
+                self.set('newContributor', '');
+            }
+
+            var findNewModel  = function () {
+                self.store.findRecord('workspace', wid, { reload: true }).then(refreshModel);
+            }
+
+            var addContributor = function (user) {
+                $.post('http://localhost:8080/api/users/'+ user.id +'/workspaces/'+ wid +'/subscribe', findNewModel);
+            }
+
+            var orFailGracefully = function () {
+                console.log('email not found');
+            }
+
+            this.store.findRecord('user', email).then(addContributor, orFailGracefully);
         }
     }
 });
