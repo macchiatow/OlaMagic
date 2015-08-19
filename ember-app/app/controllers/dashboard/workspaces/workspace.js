a2552b7c6e59038d210855a08c75ff861157ebba
Togrul Mageramov <cmeisters@gmail.com>
2015-08-19 09:19:51 +0300

    * Created workspace owner section

--- ember-app/app/controllers/dashboard/workspaces/workspace.js
+++ ember-app/app/controllers/dashboard/workspaces/workspace.js
@@ -4,9 +4,37 @@ export default Ember.Controller.extend({
 
     renameDisabled: true,
 
+    isOwner: function() {
+        var owner = this.get('model').get('owner')
+        return owner == 'togrul1@web.de';
+    }.property('model.id'),
+
     actions: {
         renameEnable: function () {
             this.set('renameDisabled', false);
+        },
+
+        unsubscribeWorkspace: function (uid, wid) {
+            var self = this;
+
+            var refreshModel = function(newModel) {
+                self.set('model', newModel);
+                self.set('newContributor', '');
+            }
+
+            var findNewModel  = function () {
+                self.store.findRecord('workspace', wid, { reload: true }).then(refreshModel);
+            }
+
+            var addContributor = function (user) {
+                $.post('http://localhost:8080/api/users/'+ uid +'/workspaces/'+ wid + '/unsubscribe', findNewModel);
+            }
+
+            var orFailGracefully = function () {
+                console.log('email not found');
+            }
+
+            this.store.findRecord('user', email).then(addContributor, orFailGracefully);
         },
 
         renameWorkspace: function (newTitle, id) {
