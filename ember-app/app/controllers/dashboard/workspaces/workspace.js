9eaffa80b55979b5ddc01dbc2880e0d0fd3ad7f6
Togrul Mageramov <cmeisters@gmail.com>
2015-08-24 17:48:34 +0200

    * Workspace controller refactor part 1

--- ember-app/app/controllers/dashboard/workspaces/workspace.js
+++ ember-app/app/controllers/dashboard/workspaces/workspace.js
@@ -46,21 +46,19 @@ export default Ember.Controller.extend({
 
         },
 
-        addContributor: function (email, wid) {
+        addContributor: function (email) {
+            var user;
             var self = this;
+            var model = this.get('model');
 
-            var refreshModel = function(newModel) {
-                self.set('model', newModel);
+            var refreshModel  = function () {
                 self.set('newContributor', '');
-            }
-
-            var findNewModel  = function () {
-                self.store.findRecord('workspace', wid, { reload: true }).then(refreshModel);
+                model.get('contributors').addObject(user);
             }
 
             var addContributor = function (users) {
-                var uid = users.get('firstObject').id;
-                $.post(self.get('host') + '/api/users/'+ uid +'/workspaces/'+ wid +'/subscribe', findNewModel);
+                user = users.get('firstObject');
+                $.post(self.get('host') + '/api/users/' + user.id +'/workspaces/' + model.id + '/subscribe', refreshModel);
             }
 
             var orFailGracefully = function () {
@@ -93,41 +91,29 @@ export default Ember.Controller.extend({
             this.store.query('user', {email: user.get('email')}).then(addContributor, orFailGracefully);
         },
 
-        unsubscribeContributor: function (wid) {
+        unsubscribeContributor: function () {
             var self = this;
-            var _user;
-
-            var removeFromContributors = function(workspace) {
-                self.controllerFor('dashboard.workspaces').set('activeWorkspace', null);
-                self.transitionToRoute('dashboard.workspaces.all');
-                _user.get('workspacesContributing').removeObject(workspace);
-            }
-
-            var findWorkspace  = function () {
-                self.store.find('workspace', wid).then(removeFromContributors);
-            }
-
-            var unsubscribeContributor = function (user) {
-                _user = user;
-                $.post(self.get('host') + '/api/users/' + user.id +'/workspaces/'+ wid +'/unsubscribe', findWorkspace);
-            }
-
-            var orFailGracefully = function () {
-                console.log('email not found');
-            }
-
-            this.store.find('user', this.get('session.userId')).then(unsubscribeContributor, orFailGracefully);
-        },
-
-        changeOwner: function () {
-            var self = this;
-            var user = this.get('session.user');
+            var currentUser = this.get('session.user');
             var model = this.get('model');
 
             var removeFromContributors = function() {
                 self.controllerFor('dashboard.workspaces').set('activeWorkspace', null);
                 self.transitionToRoute('dashboard.workspaces.all');
-                user.get('workspacesOwning').removeObject(model);
+                currentUser.get('workspacesContributing').removeObject(model);
+            }
+
+            $.post(this.get('host') + '/api/users/' + this.get('session.userId') +'/workspaces/'+ this.get('model.id') +'/unsubscribe', removeFromContributors);
+        },
+
+        changeOwner: function () {
+            var self = this;
+            var currentUser = this.get('session.user');
+            var model = this.get('model');
+
+            var removeFromContributors = function() {
+                self.controllerFor('dashboard.workspaces').set('activeWorkspace', null);
+                self.transitionToRoute('dashboard.workspaces.all');
+                currentUser.get('workspacesOwning').removeObject(model);
             }
 
             $.post(self.get('host') + '/api/users/' + this.get('newOwner') +'/workspaces/'+ this.get('model.id') +'/change_owner', removeFromContributors);
