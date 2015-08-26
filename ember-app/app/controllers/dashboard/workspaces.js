515d6d4498bfecdc0ef897422c5e881e49cff03f
Togrul Mageramov <cmeisters@gmail.com>
2015-08-26 22:28:50 +0200

    * Playing with RSVP Promises (part 2)

--- ember-app/app/controllers/dashboard/workspaces.js
+++ ember-app/app/controllers/dashboard/workspaces.js
@@ -1,29 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
     session: Ember.inject.service('session'),
 
     actions: {
         newWorkspace: function () {
-            var userId = this.get('session.userId');
             var self = this;
-            var _workspace;
+            var user = this.get('session.user');
 
-            var updateModel = function(model){
-                self.set('model', model);
-                self.transitionToRoute('dashboard.workspaces.workspace', _workspace.id);
-            }
-
-            var queryStore = function(workspace) {
-                _workspace = workspace;
-                self.store.find('user', userId, { reload: true }).then(updateModel);
-            };
-
-            this.store.find('user', userId).then(function(user) {
-                self.store.createRecord('workspace', {owner: user}).save().then(queryStore);
-            })
-
+            this.store.createRecord('workspace', {owner: user})
+                .save()
+                .then(function(workspace) {
+                    user.get('workspacesOwning').addObject(workspace);
+                    self.transitionToRoute('dashboard.workspaces.workspace', workspace.id);
+                })
         }
     }
 });
