34fd1a41b7df9c37db71baa9e30a085dc272833d
Togrul Mageramov <cmeisters@gmail.com>
2015-09-19 07:59:37 +0300

    * Fixed active-workspace service behaviour at login

--- ember-app/app/controllers/login.js
+++ ember-app/app/controllers/login.js
@@ -3,11 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
     actions: {
-        authenticate: function() {
+        authenticate: function () {
+            const self = this;
             var credentials = this.getProperties('identification', 'password');
             var authenticator = 'simple-auth-authenticator:oauth2-password-grant';
-            this.get('session').authenticate(authenticator, credentials);
-            this.get('active-workspace').clean();
+            this.get('session').authenticate(authenticator, credentials)
+                .then(function () {
+                    self.get('active-workspace').clean();
+                });
         }
     }
 });
