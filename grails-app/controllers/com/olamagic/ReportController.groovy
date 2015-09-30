1893b7eaec024e2f1e923599b5ac6f95042ef92a
Togrul Mageramov <cmeisters@gmail.com>
2015-09-30 08:31:31 +0300

    * Attempt to generate a report (part 2)

--- grails-app/controllers/com/olamagic/ReportController.groovy
+++ grails-app/controllers/com/olamagic/ReportController.groovy
@@ -4,8 +4,9 @@ import grails.converters.JSON
 
 class ReportController {
 
-    def generate(){
-        render ([report: [id: 1, a: []]] as JSON)
+    def generate() {
+        def result = Call.executeQuery('select n.upid, a.name from Call c, AdSource a, Number n where c.number = n.id and n.adSource = a.id')
+        render ([reports: [[id: 1, a: [result]]]] as JSON)
     }
 
 }
