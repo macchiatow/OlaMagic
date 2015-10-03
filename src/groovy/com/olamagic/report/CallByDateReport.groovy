7cf2f8fe8d6e30ac1ac7b751c99b911b81966392
Togrul Mageramov <cmeisters@gmail.com>
2015-10-04 00:05:46 +0300

    * Attempt to generate a report (part 6)

--- src/groovy/com/olamagic/report/CallByDateReport.groovy
+++ src/groovy/com/olamagic/report/CallByDateReport.groovy
@@ -1,6 +1,8 @@
 package com.olamagic.report
 
 import com.olamagic.Call
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 /**
  * Created by togrul on 9/30/15.
@@ -9,8 +11,20 @@ class CallByDateReport {
 
     def generate(){
         Call.withSession { session ->
-            def q = session.createSQLQuery("select to_char(date, 'DD-MM-YYYY'), count(*) from Call GROUP BY to_char(date, 'DD-MM-YYYY')").list()
-            [x:q.reverse().collect {it[0]}, y:q.reverse().collect{it[1]}]
+            def q = session.createSQLQuery("select to_char(date, 'DDMMYYYY'), count(*) from Call GROUP BY to_char(date, 'DDMMYYYY')").list()
+
+            def f = DateTimeFormat.forPattern("ddMMyyyy");
+            def qMap = q.collectEntries { [(f.parseDateTime(it[0])):it[1]] }
+
+            DateTime startDay = new DateTime().minusDays(7).withMillisOfDay(0)
+            while (startDay.beforeNow) {
+                startDay = startDay.plusDays(1)
+                qMap.putIfAbsent(startDay, 0);
+            }
+
+            qMap = qMap.sort({ a, b -> a.key <=> b.key })
+
+            [x:qMap.collect {it.key.toString(f)}, y:qMap.collect{it.value}]
         }
     }
 }
