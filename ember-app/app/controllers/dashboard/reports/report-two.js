2f07f7dcb3039522476d032cb69dc7d4b4b38a73
Togrul Mageramov <cmeisters@gmail.com>
2015-10-05 09:09:22 +0300

    * Set proper chart legend

--- ember-app/app/controllers/dashboard/reports/report-two.js
+++ ember-app/app/controllers/dashboard/reports/report-two.js
@@ -2,29 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-    report: function () {
-        return this.get('model.firstObject.a');
-    }.property('model'),
-
-    options: {
+    chartOptions: {
         datasetFill: false,
-        bezierCurve: false,
-        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+        bezierCurveTension: 0.2,
+        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li style=\"color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
     },
-
-    data: {},
 
     init: function () {
         var self = this;
         self.store.query('report', {type: 3}).then(function (reports) {
-            var parsed = {
+            var chartData = {
                 labels: reports.get('firstObject.a.x'),
                 datasets: []
             };
 
             for (var i = 0; i < reports.get('firstObject.a.y').length; i++) {
                 var color = self._random_color();
-
                 var lineData = reports.get('firstObject.a.y')[i];
                 var line = {
                     label: lineData[0],
@@ -35,13 +28,19 @@ export default Ember.Controller.extend({
                     pointHighlightStroke: "rgba(220,220,220,1)",
                     data: lineData[1]
                 };
-                parsed.datasets.push(line);
+                chartData.datasets.push(line);
             }
 
-            self.set('data', parsed)
-        })
-    },
+            self.set('chartData', chartData)
+            self.set('options', {
+                datasetFill: false,
+                bezierCurveTension: 0.2,
+                legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li style=\"color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+            })
 
+
+        });
+    },
 
     _random_color: function () {
         function c() {
@@ -49,7 +48,6 @@ export default Ember.Controller.extend({
         }
 
         return "rgba(" + [c(), c(), c(), 1].join(',') + ")";
-    },
+    }
 
-    actions: {}
 });
