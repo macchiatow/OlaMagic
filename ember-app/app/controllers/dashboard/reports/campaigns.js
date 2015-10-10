bd9dae7039ff24833f24c87d268574d8fe5e689e
Togrul Mageramov <cmeisters@gmail.com>
2015-10-10 22:49:55 +0300

    * Attempt to generate a report (part 18)

--- ember-app/app/controllers/dashboard/reports/campaigns.js
+++ ember-app/app/controllers/dashboard/reports/campaigns.js
@@ -15,35 +15,83 @@ export default Ember.Controller.extend({
         legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li style=\"color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
     },
 
-    init: function () {
+    _updateChart: function () {
         var self = this;
-        self.store.query('report', {type: 'campaign'}).then(function (reports) {
-            var tableData = [];
-            var chartData = {
-                labels: reports.get('firstObject.a.x'),
-                datasets: []
-            };
-
-            for (var i = 0; i < reports.get('firstObject.a.y').length; i++) {
-                var color = self._random_color();
-                var lineData = reports.get('firstObject.a.y')[i];
-                var line = {
-                    label: lineData[0],
-                    strokeColor: color,
-                    pointColor: color,
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: lineData[1]
+        self.store.query('report', {
+                type: 'campaign',
+                detalization: 'hours',
+                rangeFrom: this.get('rangeFrom'),
+                rangeTo: this.get('rangeTo')
+            }).then(function (reports) {
+                var tableData = [];
+                var chartData = {
+                    labels: reports.get('firstObject.a.x'),
+                    datasets: []
                 };
 
-                chartData.datasets.push(line);
-                tableData.push({campaign:lineData[0], count: eval(lineData[1].join('+'))});
-            }
+                for (var i = 0; i < reports.get('firstObject.a.y').length; i++) {
+                    var color = self._random_color();
+                    var lineData = reports.get('firstObject.a.y')[i];
+                    var line = {
+                        label: lineData[0],
+                        strokeColor: color,
+                        pointColor: color,
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(220,220,220,1)",
+                        data: lineData[1]
+                    };
 
-            self.set('chartData', chartData);
-            self.set('tableData', tableData)
-        });
+                    chartData.datasets.push(line);
+                    tableData.push({campaign: lineData[0], count: eval(lineData[1].join('+'))});
+                }
+
+                self.set('chartData', chartData);
+                self.set('tableData', tableData)
+            });
+    },
+
+    actions: {
+
+        detalizeToday: function () {
+            this.set('rangeFrom', Date.today().getTime());
+            this.set('rangeTo', Date.today().addDays(1).getTime());
+            this.set('detalization', 'hours');
+
+            this._updateChart();
+        },
+
+        detalizeYesterday: function () {
+            this.set('rangeFrom', Date.today().addDays(-1).getTime());
+            this.set('rangeTo', Date.today().getTime());
+            this.set('detalization', 'hours');
+
+            this._updateChart();
+        },
+
+        detalizeWeek: function () {
+            this.set('rangeFrom', Date.today().addWeeks(-1).addDays(1).getTime());
+            this.set('rangeTo', Date.today().addDays(1).getTime());
+            this.set('detalization', 'days');
+
+            this._updateChart();
+        },
+
+        detalizeMonth: function () {
+            this.set('rangeFrom', Date.today().addMonths(-1).getTime());
+            this.set('rangeTo', Date.today().addDays(1).getTime());
+            this.set('detalization', 'weeks');
+
+            this._updateChart();
+        },
+
+        detalizeYear: function () {
+            this.set('rangeFrom', Date.today().addYears(-1).getTime());
+            this.set('rangeTo', Date.today().addDays(1).getTime());
+            this.set('detalization', 'weeks');
+
+            this._updateChart();
+        }
     },
 
     _random_color: function () {
--- src/groovy/com/olamagic/report/Campaign.groovy
+++ src/groovy/com/olamagic/report/Campaign.groovy
@@ -10,23 +10,48 @@ import org.joda.time.format.DateTimeFormat
 class Campaign {
 
     def generate(Long rangeFrom, Long rangeTo, String detalization){
-        def f = DateTimeFormat.forPattern("ddMMyyyy");
+        Call.withSession {
+            if (detalization == 'hours') {
+                query(it, rangeFrom, rangeTo, new DateTime(rangeFrom), 'plusHours', 'DDMMYYYY', 'ddMMyyyy', 'HH:mm')
 
-        Call.withSession { session ->
-            def q = session.createSQLQuery("select ifnull(a.name,'undefined'), to_char(c.date, 'DDMMYYYY'), count(*) from Call c INNER JOIN Number n ON c.number_id=n.id LEFT OUTER JOIN AD_SOURCE a ON n.AD_SOURCE_ID=a.id GROUP BY a.name, to_char(c.date, 'DDMMYYYY')")
-                    .list().sort {a,b -> f.parseDateTime(a[1]) <=> f.parseDateTime(b[1])}
+            } else if (detalization == 'months') {
+                query(it, rangeFrom, rangeTo, new DateTime(rangeFrom).withDayOfMonth(1), 'plusMonths', 'Mon YYYY', 'MMM YYYY', 'MMM YYYY')
 
-            def campaigns = q.collectEntries {[(it[0]):[]]}
-            def dates = q.collect({it[1]}).unique()
+            } else if (detalization == 'weeks') {
+                query(it, rangeFrom, rangeTo, new DateTime(rangeFrom).withDayOfWeek(1), 'plusWeeks', 'WW YYYY', 'w y', 'dd-MMM')
 
-            campaigns.each( { campaign,v ->
-                dates.each({day ->
-                    def found =  q.find ({qi-> qi[0] == campaign && qi[1] == day})
-                    v << (found? found[2] : 0)
-                })
-            })
+            } else if (detalization == 'days') {
+                query(it, rangeFrom, rangeTo, new DateTime(rangeFrom), 'plusDays', 'DDMMYYYY', 'ddMMyyyy', 'dd-MM-yyyy')
 
-            [x:q.collect({it[1]}).unique(), y:campaigns.collect { [it.key, it.value] }]
+            } else {
+                it.createSQLQuery("select c.date as dates, n.upid, c.duration, ifnull(a.name,'undefined') from Call c INNER JOIN Number n ON c.number_id=n.id LEFT OUTER JOIN AD_SOURCE a ON n.AD_SOURCE_ID=a.id where datediff(s, '1970-01-01', date) * 1000 between $rangeFrom and $rangeTo").list()
+            }
         }
     }
+
+    def query(def session, def rangeFrom, def rangeTo, def startFrom, def incrementer, def groupByPattern, def parsePattern, def keyPattern) {
+        def f = DateTimeFormat.forPattern(parsePattern);
+
+        def q = session.createSQLQuery("select ifnull(a.name,'undefined'), to_char(c.date, '$groupByPattern'), count(*) from Call c INNER JOIN Number n ON c.number_id=n.id LEFT OUTER JOIN AD_SOURCE a ON n.AD_SOURCE_ID=a.id where datediff(s, '1970-01-01', date) * 1000 between $rangeFrom and $rangeTo GROUP BY a.name, to_char(c.date, '$groupByPattern')")
+                .list().sort {a,b -> f.parseDateTime(a[1]) <=> f.parseDateTime(b[1])}
+
+        def campaigns = q.collectEntries {[(it[0]):[]]}
+        def dates = []
+
+        def start = new DateTime(rangeFrom)
+        while (start.isBefore(rangeTo)) {
+            dates << start;
+            start = start."$incrementer"(1)
+        }
+
+        campaigns.each( { campaign,v ->
+            dates.collect({it.toString('ddMMyyyy')}).unique().each({day ->
+                def found =  q.find ({qi-> qi[0] == campaign && qi[1] == day})
+                v << (found? found[2] : 0)
+            })
+        })
+
+        [x:dates.collect({it.toString('ddMMyyyy')}).unique(), y:campaigns.collect { [it.key, it.value] }]
+    }
+
 }
